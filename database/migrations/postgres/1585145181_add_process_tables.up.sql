449dbbb7dfff1ce7e7fe82d4ad506a87df84a671
Nandor Kracser <bonifaido@gmail.com>
2020-04-01 15:34:03 +0200

    fix postgres migrations
    Signed-off-by: Nandor Kracser <bonifaido@gmail.com>

--- database/migrations/postgres/1585145181_add_process_tables.up.sql
+++ database/migrations/postgres/1585145181_add_process_tables.up.sql
@@ -11,13 +11,20 @@ CREATE TABLE "public"."processes" (
     PRIMARY KEY ("id")
 );
 
-CREATE INDEX idx_start_time_and_time ON "processes"("started_at", "finished_at");
+CREATE INDEX idx_processes_parent_id ON processes USING btree (parent_id);
+
+CREATE INDEX idx_start_time_end_time ON processes USING btree (started_at, finished_at);
 
 CREATE TABLE "public"."process_events" (
-    "process_id" text,
+    "id" serial,
+    "process_id" text NOT NULL,
     "log" text NOT NULL,
     "name" text NOT NULL,
     "status" text NOT NULL,
     "timestamp" timestamptz NOT NULL DEFAULT now(),
     CONSTRAINT "process_events_process_id_processes_id_foreign" FOREIGN KEY ("process_id") REFERENCES "public"."processes"("id") ON DELETE RESTRICT ON UPDATE RESTRICT
 );
+
+CREATE UNIQUE INDEX process_events_pkey ON process_events USING btree (id);
+
+ALTER TABLE "process_events" add constraint "process_events_pkey" PRIMARY KEY using index "process_events_pkey";
